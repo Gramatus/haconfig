84a98f9cbef0b6382686b6d54777ef52da0c8ea2
root <root@a0d7b954-ssh.local.hass.io>
2021-05-16 20:05:48 +0200

    Added back my custom code

--- custom_components/spotcast/__init__.py
+++ custom_components/spotcast/__init__.py
@@ -307,21 +307,78 @@ def setup(hass, config):
         )
         if uri.find("show") > 0:
             show_episodes_info = client.show_episodes(uri)
-            if show_episodes_info and len(show_episodes_info["items"]) > 0:
-                if ignore_fully_played:
-                    for episode in show_episodes_info["items"]:
-                        if not episode["resume_point"]["fully_played"]:
-                            episode_uri = episode["external_urls"]["spotify"]
-                            break
-                else:
-                    episode_uri = show_episodes_info["items"][0]["external_urls"][
-                        "spotify"
-                    ]
+            readCount = len(show_episodes_info["items"])
+            _LOGGER.debug("Readcount: %s",readCount)
+            lastEpisode = show_episodes_info["items"][0];
+            episodeToPlay = None
+            for episode in show_episodes_info["items"]:
+                if episodeToPlay is None:
+                    playState = episode["resume_point"]
+                    if playState["fully_played"]:
+                        episodeToPlay = lastEpisode
+                        _LOGGER.debug("Found that this is fully played: %s",episode)
+                    lastEpisode = episode
+            if episodeToPlay is None:
+                show_episodes_info = client.show_episodes(uri,50,readCount)
+                readCount = readCount+len(show_episodes_info["items"])
+                _LOGGER.debug("Readcount: %s",readCount)
+                for episode in show_episodes_info["items"]:
+                    if episodeToPlay is None:
+                        playState = episode["resume_point"]
+                        if playState["fully_played"]:
+                            episodeToPlay = lastEpisode
+                            _LOGGER.debug("Found that this is fully played: %s",episode)
+                        lastEpisode = episode
+            if episodeToPlay is None:
+                show_episodes_info = client.show_episodes(uri,50,readCount)
+                readCount = readCount+len(show_episodes_info["items"])
+                _LOGGER.debug("Readcount: %s",readCount)
+                for episode in show_episodes_info["items"]:
+                    if episodeToPlay is None:
+                        playState = episode["resume_point"]
+                        if playState["fully_played"]:
+                            episodeToPlay = lastEpisode
+                            _LOGGER.debug("Found that this is fully played: %s",episode)
+                        lastEpisode = episode
+            if episodeToPlay is None:
+                show_episodes_info = client.show_episodes(uri,50,readCount)
+                readCount = readCount+len(show_episodes_info["items"])
+                _LOGGER.debug("Readcount: %s",readCount)
+                for episode in show_episodes_info["items"]:
+                    if episodeToPlay is None:
+                        playState = episode["resume_point"]
+                        if playState["fully_played"]:
+                            episodeToPlay = lastEpisode
+                            _LOGGER.debug("Found that this is fully played: %s",episode)
+                        lastEpisode = episode
+            if episodeToPlay is None:
+                show_episodes_info = client.show_episodes(uri,50,readCount)
+                readCount = readCount+len(show_episodes_info["items"])
+                _LOGGER.debug("Readcount: %s",readCount)
+                for episode in show_episodes_info["items"]:
+                    if episodeToPlay is None:
+                        playState = episode["resume_point"]
+                        if playState["fully_played"]:
+                            episodeToPlay = lastEpisode
+                            _LOGGER.debug("Found that this is fully played: %s",episode)
+                        lastEpisode = episode
+            if episodeToPlay is not None:
+                _LOGGER.debug("episodeToPlay (found to be the oldest one not yet fully played): %s",episodeToPlay)
+                episode_uri = episodeToPlay["external_urls"]["spotify"]
+                _LOGGER.debug(
+                    "Playing episode using uris (latest podcast playlist)= for uri: %s",
+                    episode_uri,
+                )
+                client.start_playback(device_id=spotify_device_id, uris=[episode_uri])                
+            elif show_episodes_info and len(show_episodes_info["items"]) < 0:
+                episode_uri = show_episodes_info["items"][0]["external_urls"]["spotify"]
                 _LOGGER.debug(
                     "Playing episode using uris (latest podcast playlist)= for uri: %s",
                     episode_uri,
                 )
                 client.start_playback(device_id=spotify_device_id, uris=[episode_uri])
+            else:
+                _LOGGER.info("Umm...")
         elif uri.find("episode") > 0:
             _LOGGER.debug("Playing episode using uris= for uri: %s", uri)
             client.start_playback(device_id=spotify_device_id, uris=[uri])
