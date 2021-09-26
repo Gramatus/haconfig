e4ffbb8bd645a31e696d9a8aee1a3cc0e419ef6a
root <root@a0d7b954-ssh.local.hass.io>
2021-09-26 19:13:03 +0200

    Added more playlists and some other improvements to remote actions

--- pyscript/scripts/remote_actions.py
+++ pyscript/scripts/remote_actions.py
@@ -1,6 +1,7 @@
 import logging
 import json
 import re
+import media_services
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,11 +31,26 @@ fields:
     if len(matches) == 0:
         return
     action = matches[0]["action"]
+    shuffle_type = state.get("input_select.quick_play_shuffle_type")
     if action == None:
         _LOGGER.debug("Receieved a keypress that has no action connected to it")
         return
     if action == "Spill Alle Gromlåter":
-        _LOGGER.info("TODO: Trigger playing music...")
+        pyscript.play_playlist_random(playlistid="2pjB7wGkkoG9VYY8enMR5b", shuffle_type=shuffle_type)
+    elif action == "Spill Kristne gromlåter":
+        pyscript.play_playlist_random(playlistid="4dgE3OmDZxl5OOj5SSriWX", shuffle_type=shuffle_type)
+    elif action == "Spill Classical essentials":
+        pyscript.play_playlist_random(playlistid="37i9dQZF1DWWEJlAGA9gs0", shuffle_type=shuffle_type)
+    elif action == "Spill Bluesfavoritter":
+        pyscript.play_playlist_random(playlistid="0C2U8SFwZ3Y9bBjVa2KSMx", shuffle_type=shuffle_type)
+    elif action == "Spill Night blues":
+        pyscript.play_playlist_random(playlistid="2VJTrnLKqMyKZnnPXh3Ttt", shuffle_type=shuffle_type)
+    elif action == "Spill Jazz Guitar":
+        pyscript.play_playlist_random(playlistid="5xddIVAtLrZKtt4YGLM1SQ", shuffle_type=shuffle_type)
+    elif action == "Spill 90's Country":
+        pyscript.play_playlist_random(playlistid="37i9dQZF1DWVpjAJGB70vU", shuffle_type=shuffle_type)
+    elif action == "Spill Visefavoritter":
+        pyscript.play_playlist_random(playlistid="58TtpzdDAFoFkdTHXbx2ak", shuffle_type=shuffle_type)
     elif action == "NRK P1":
         _LOGGER.info("TODO: Trigger action \"" + action + "\"...")
     elif action == "Tving HA med bryter":
@@ -43,8 +59,26 @@ fields:
         _LOGGER.info("TODO: Trigger action \"" + action + "\"...")
     elif action == "Forrige på Spotify":
         _LOGGER.info("TODO: Trigger action \"" + action + "\"...")
-    elif action == "Play/pause Spotify":
-        _LOGGER.info("TODO: Trigger action \"" + action + "\"...")
+    elif action == "Play media":
+        playingEntity, playState = media_services.getPlayingEntity()
+        if playingEntity is not None:
+            if playState == "playing":
+                _LOGGER.info("Pausing " + playingEntity)
+                media_player.media_pause(entity_id=playingEntity)
+            elif playState == "paused":
+                _LOGGER.info("Resuming " + playingEntity)
+                media_player.media_play(entity_id=playingEntity)
+    elif action == "Pause media":
+        playingEntity, playState = media_services.getPlayingEntity()
+        if playingEntity is not None:
+            if playState == "playing":
+                _LOGGER.info("Pausing " + playingEntity)
+                media_player.media_pause(entity_id=playingEntity)
+    elif action == "Stop media":
+        playingEntity, playState = media_services.getPlayingEntity()
+        if playingEntity is not None:
+            _LOGGER.info("Stopping " + playingEntity)
+            media_player.media_stop(entity_id=playingEntity)
     elif action == "Neste på Spotify":
         _LOGGER.info("TODO: Trigger action \"" + action + "\"...")
     elif action == "Anlegg kjøkken av/på":
@@ -57,3 +91,8 @@ fields:
         _LOGGER.info("TODO: Trigger action \"" + action + "\"...")
     else:
         _LOGGER-warning("Received action \"" + action + "\", but no way to handle the action has been defined")
+
+@service
+def getPlayingEntity():
+    playingEntity, playState = media_services.getPlayingEntity()
+    _LOGGER.info(playingEntity)
