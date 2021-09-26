8e096a8f8412cbbc71d43b828de7e903fecf2257
root <root@a0d7b954-ssh.local.hass.io>
2021-09-26 15:07:17 +0200

    New version of custom components

--- custom_components/gramatus/__init__.py
+++ custom_components/gramatus/__init__.py
@@ -24,11 +24,53 @@ def setup(hass, config):
     hass.states.set("fully.nettbrett2", "on", {"friendly_name": "Fully på nettbrett 2", "icon": "mdi:tablet-dashboard", "ip": "192.168.50.108"})
     hass.states.set("fully.lgg3", "on", {"friendly_name": "Fully på LG G3", "icon": "mdi:tablet-dashboard", "ip": "192.168.50.149"})
 
+    # Transition entities
+    _LOGGER.info(hass.states.get("hue_transitions.Kveldslys"))
+    if hass.states.get("hue_transitions.Kveldslys") == None:
+        hass.states.set("hue_transitions.Kveldslys","on",{
+            "Scenes": [
+                {
+                    "index": 1,
+                    "name": "FadeKveldslys !start",
+                    "id": "KMVf61DE0jXA9b4",
+                    "timeinseconds": (60*60)+(0)
+                },
+                {
+                    "index": 2,
+                    "name": "FadeKveldslys 1t0min",
+                    "id": "fVqEsKelwr0BLv3",
+                    "timeinseconds": (60*60)+(0)
+                },
+                {
+                    "index": 4,
+                    "name": "FadeKveldslys 2t30min",
+                    "id": "uofrddCzHp-x0vF",
+                    "timeinseconds": (30*60)+(0)
+                },
+                {
+                    "index": 3,
+                    "name": "FadeKveldslys 2t0min",
+                    "id": "rtH7xu7qvsv3PPv",
+                    "timeinseconds": (30*60)+(0)
+                },
+                {
+                    "index": 5,
+                    "name": "FadeKveldslys 3t0min",
+                    "id": "iVcGM9UbJtc34TF",
+                    "timeinseconds": (30*60)+(0)
+                }
+            ],
+            "Rooms": [
+                "light.bad",
+                "light.kontor"
+            ]
+        })
+
     def handle_test():
         """Handle the test."""
         _LOGGER.debug("Gramatus testing")
 
     #hass.services.register(GRAMATUS_DOMAIN, "gramatus_test", handle_test)
 
-    # Return boolean to indicate that initialization was successfully.
+    # Return boolean to indicate that initialization was successful
     return True
